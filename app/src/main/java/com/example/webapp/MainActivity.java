0a32b4957e576049062743a53c72a4c879654133
Firefly <firefly@example.com>
2026-05-29 18:48:38 +0800

    Fix missing InputStream import

--- app/src/main/java/com/example/webapp/MainActivity.java
+++ app/src/main/java/com/example/webapp/MainActivity.java
@@ -23,6 +23,7 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
