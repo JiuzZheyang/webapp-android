952126adb6cb330b289160edec14b742a0d737bc
Firefly <firefly@example.com>
2026-05-29 18:50:27 +0800

    Fix missing OutputStream import

--- app/src/main/java/com/example/webapp/MainActivity.java
+++ app/src/main/java/com/example/webapp/MainActivity.java
@@ -26,6 +26,7 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.io.Serializable;
 import java.net.HttpURLConnection;
 import java.net.URL;
